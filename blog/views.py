765d5845f549e1c3c2e58eddb6c83fb9eb00e2a9
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-01 23:40:54 +0300

    chore: clean up entire project for fresh restart

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-
-
-def index(request):
-    return render(request, 'blog/index.html', context={'site': 'mysite.com'})
-
-
-def contact(request):
-    return redirect('blog:about')
