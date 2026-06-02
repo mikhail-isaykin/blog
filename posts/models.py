617d7c10f9b30301733b8687b3b54e4eb7b98471
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-02 23:54:47 +0300

    feat: add Classroom model with name, code and timestamps

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 
-class Post(models.Model):
-    text = models.TextField()
-
+class Classroom(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=11, unique=True)
+    description = models.TextField(blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
-        return self.text[:50]
+        return self.name
