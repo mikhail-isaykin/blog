40bdde50f644a05e03caff8429d3f7eab8cecd2f
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-09 23:44:50 +0300

    feat: register Comment model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from .models import Comment, Post
+
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status', 'created']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
