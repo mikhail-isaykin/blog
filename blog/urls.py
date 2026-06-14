c6c9d9a5c99b08fef697c0e3066f9a3a82a1e016
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-14 23:55:48 +0300

    feat: add search URL route

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PostListView, post_comment, post_detail, post_share
+from .views import PostListView, post_comment, post_detail, post_search, post_share
 
 app_name = 'blog'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<int:pk>/share/', post_share, name='post_share'),
     path('<int:pk>/comment/', post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', PostListView.as_view(), name='post_list_by_tag'),
+    path('search/', post_search, name='post_search'),
 ]
