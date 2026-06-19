2924078675442acb393a039e8bd498cf533b683c
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-19 23:43:49 +0300

    feat: add login and logout url routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import SignUpView
+from .views import CustomLoginView, SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
