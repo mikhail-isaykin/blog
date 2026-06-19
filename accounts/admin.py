8ae48dc96d828a3b17288dd9afff1f392cac7319
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-19 23:22:34 +0300

    feat: customize user admin with unfold ModelAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+User = get_user_model()
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_display_links = ['username']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups']
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+    ordering = ['username']
+    filter_horizontal = ['groups', 'user_permissions']
