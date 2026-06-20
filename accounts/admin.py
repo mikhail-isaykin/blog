e81f7dfcfc74ef5f4477eef6955c8e31846f6a20
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-20 23:44:51 +0300

    feat: register GroupAdmin and ProfileAdmin in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from .models import Profile
 from unfold.admin import ModelAdmin
 
 User = get_user_model()
 
 admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
 @admin.register(User)
@@ -15,3 +18,19 @@ class UserAdmin(ModelAdmin):
     search_fields = ['username', 'first_name', 'last_name', 'email']
     ordering = ['username']
     filter_horizontal = ['groups', 'user_permissions']
+
+
+@admin.register(Group)
+class GroupAdmin(ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+    filter_horizontal = ['permissions']
+
+
+@admin.register(Profile)
+class ProfileAdmin(ModelAdmin):
+    list_display = ['user', 'bio']
+    list_display_links = ['user']
+    search_fields = ['user__username', 'bio']
+    raw_id_fields = ['user']
