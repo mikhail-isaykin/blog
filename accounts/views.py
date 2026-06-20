dbde08eed3dce8a4e3ef26d992082b692152304a
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-20 23:43:16 +0300

    feat: add profile view and password change view

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth import login
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import View
 
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, SignUpForm, UpdateProfileForm, UpdateUserForm
 
 
 class SignUpView(View):
@@ -23,29 +26,53 @@ class SignUpView(View):
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
-
         if form.is_valid():
             user = form.save()
             login(request, user)
-
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
-
             return redirect(to='blog:post_list')
-
         return render(request, self.template_name, {'form': form})
 
 
 class CustomLoginView(LoginView):
     form_class = LoginForm
-    redirect_authenticated_user=True
-    template_name='registration/login.html'
-    
+    redirect_authenticated_user = True
+    template_name = 'registration/login.html'
+
     def form_valid(self, form):
         remember_me = form.cleaned_data.get('remember_me')
-        
         if not remember_me:
             self.request.session.set_expiry(0)
             self.request.session.modified = True
-            
         return super().form_valid(form)
+
+
+@login_required
+def profile(request):
+    user_form = UpdateUserForm(instance=request.user)
+    profile_form = UpdateProfileForm(instance=request.user.profile)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='accounts:profile')
+    return render(
+        request,
+        'registration/profile.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form,
+        },
+    )
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'registration/password_change_form.html'
+    success_message = 'Successfully Changed Your Password'
+    success_url = reverse_lazy('accounts:profile')
+    
