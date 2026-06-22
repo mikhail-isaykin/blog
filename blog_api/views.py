db84934972ee982c0c448e5ef3192b357b010775
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-22 23:56:18 +0300

    feat(blog_api): add post list/detail and user posts API views

---
+++ blog_api/views.py
@@ -0,0 +1,91 @@
+from rest_framework import generics, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from blog.models import Post
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+
+
+'''
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+
+
+class PostDetail(APIView):
+    def get(self, request, pk, format=None):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    
+    def patch(self, request, pk, format=None):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    
+    def delete(self, request, pk, format=None):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT,
+        )
+'''
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    # filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author']
+    
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserPostList(generics.ListAPIView):
+    serializer_class = PostSerializer
+    
+    def get_queryset(self):
+        user = self.kwargs.get('id')
+        return Post.objects.filter(author=user)
