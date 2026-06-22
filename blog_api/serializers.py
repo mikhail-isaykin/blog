c5e2c0c826e2c02f0de371ab2823d717dcc7b715
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-22 23:56:54 +0300

    feat(blog_api): add Post and Comment serializers

---
+++ blog_api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from blog.models import Comment, Post
+
+
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        comment = Comment.objects.create(**validated_data)
+        return comment
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+        instance.save()
+        return instance
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'author',
+            'title',
+            'body',
+            'created',
+            'status',
+            'slug',
+        ]
