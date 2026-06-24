5affa1dcdeb8244df585d8db6012db6740f1a720
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-24 23:28:49 +0300

    refactor: add widget styling to blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -4,19 +4,39 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(
-        required=False,
-        widget=forms.Textarea,
+    name = forms.CharField(
+        max_length=25,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control mb-1', 'placeholder': 'Name'}),
     )
+    email = forms.EmailField(
+        required=True, widget=forms.EmailInput(attrs={'class': 'form-control mb-1', 'placeholder': 'E-Mail'})
+    )
+    to = forms.EmailField(
+        required=True, widget=forms.EmailInput(attrs={'class': 'form-control mb-1', 'placeholder': 'To'})
+    )
+    comments = forms.CharField(
+        required=False, widget=forms.Textarea(attrs={'class': 'form-control mb-1', 'placeholder': 'Comments'})
+    )
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Текст комментария'}),
+        }
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField()
+    query = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mb-1',
+                'placeholder': 'Enter search term...',
+            },
+        ),
+    )
