a6a28b01ba6758d34cc9a81e6a0930634cc614c6
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-25 23:35:37 +0300

    feat: add accounts auth and profile urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
-from .views import CustomLoginView, SignUpView
+from .views import ChangePasswordView, CustomLoginView, SignUpView, profile
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('password_change/', ChangePasswordView.as_view(), name='password_change_form.html'),
+    path('', include('django.contrib.auth.urls')),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', profile, name='profile'),
 ]
