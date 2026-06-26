a579cb7cb47e8c8c30dffba88f4195eff32f1358
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-26 23:43:54 +0300

    test(accounts): add Profile model tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class ProfileModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='ivan', password='secret123')
+
+    def test_profile_created_for_user(self):
+        """У созданного пользователя есть профиль."""
+        self.assertIsNotNone(self.user.profile)
+
+    def test_str_returns_username(self):
+        """__str__ профиля возвращает имя пользователя."""
+        self.assertEqual(str(self.user.profile), 'ivan')
